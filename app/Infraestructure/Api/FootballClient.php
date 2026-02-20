e6598278621b2942abef82089ca6ac1deed06fe3
br <brunofragag99@gmail.com>
2026-02-20 17:39:20 +0100

    Refactor code structure for improved readability and maintainability

---
+++ app/Infraestructure/Api/FootballClient.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Infraestructure\Api;
+ class FootballClient
+{
+    public function getTeamsByName(){
+
+    }
+
+    public function getTeamById(){
+
+    }
+
+    public function getFixtures(){
+
+    }
+}
