57c0a32123f822ae8b1d82edfb1f7b9bd315972d
br <brunofragag99@gmail.com>
2026-02-21 14:43:16 +0100

    feat(): implement FavoriteTeamController with destroy functionality; add routes for favorite teams management

---
+++ resources/js/actions/App/Http/Controllers/FavoriteTeamController.ts
@@ -0,0 +1,88 @@
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
+/**
+* @see \App\Http\Controllers\FavoriteTeamController::destroy
+* @see app/Http/Controllers/FavoriteTeamController.php:15
+* @route '/favorite-teams/{teamId}'
+*/
+export const destroy = (args: { teamId: string | number } | [teamId: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
+    url: destroy.url(args, options),
+    method: 'delete',
+})
+
+destroy.definition = {
+    methods: ["delete"],
+    url: '/favorite-teams/{teamId}',
+} satisfies RouteDefinition<["delete"]>
+
+/**
+* @see \App\Http\Controllers\FavoriteTeamController::destroy
+* @see app/Http/Controllers/FavoriteTeamController.php:15
+* @route '/favorite-teams/{teamId}'
+*/
+destroy.url = (args: { teamId: string | number } | [teamId: string | number ] | string | number, options?: RouteQueryOptions) => {
+    if (typeof args === 'string' || typeof args === 'number') {
+        args = { teamId: args }
+    }
+
+    if (Array.isArray(args)) {
+        args = {
+            teamId: args[0],
+        }
+    }
+
+    args = applyUrlDefaults(args)
+
+    const parsedArgs = {
+        teamId: args.teamId,
+    }
+
+    return destroy.definition.url
+            .replace('{teamId}', parsedArgs.teamId.toString())
+            .replace(/\/+$/, '') + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\FavoriteTeamController::destroy
+* @see app/Http/Controllers/FavoriteTeamController.php:15
+* @route '/favorite-teams/{teamId}'
+*/
+destroy.delete = (args: { teamId: string | number } | [teamId: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
+    url: destroy.url(args, options),
+    method: 'delete',
+})
+
+/**
+* @see \App\Http\Controllers\FavoriteTeamController::destroy
+* @see app/Http/Controllers/FavoriteTeamController.php:15
+* @route '/favorite-teams/{teamId}'
+*/
+const destroyForm = (args: { teamId: string | number } | [teamId: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: destroy.url(args, {
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'DELETE',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'post',
+})
+
+/**
+* @see \App\Http\Controllers\FavoriteTeamController::destroy
+* @see app/Http/Controllers/FavoriteTeamController.php:15
+* @route '/favorite-teams/{teamId}'
+*/
+destroyForm.delete = (args: { teamId: string | number } | [teamId: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: destroy.url(args, {
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'DELETE',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'post',
+})
+
+destroy.form = destroyForm
+
+const FavoriteTeamController = { destroy }
+
+export default FavoriteTeamController
--- resources/js/actions/App/Http/Controllers/index.ts
+++ resources/js/actions/App/Http/Controllers/index.ts
@@ -1,8 +1,10 @@
 import DashboardController from './DashboardController'
+import FavoriteTeamController from './FavoriteTeamController'
 import Settings from './Settings'
 
 const Controllers = {
     DashboardController: Object.assign(DashboardController, DashboardController),
+    FavoriteTeamController: Object.assign(FavoriteTeamController, FavoriteTeamController),
     Settings: Object.assign(Settings, Settings),
 }
 
