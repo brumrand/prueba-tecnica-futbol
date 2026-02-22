78bea20e5d8707231347157554ae5c2505a7b3dc
br <brunofragag99@gmail.com>
2026-02-22 15:36:34 +0100

    feat(): improve dashboard test

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -16,6 +16,12 @@ class DashboardTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+
     public function test_guests_are_redirected_to_the_login_page()
     {
         $response = $this->get(route('dashboard'));
@@ -31,5 +37,44 @@ class DashboardTest extends TestCase
         $response->assertOk();
     }
 
-    
+    public function test_dashboard_shows_favorite_teams_and_matches()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $favoriteTeamIds = [529, 33];
+        $teamData1 = [
+            'team' => new TeamDTO(529, 'Barcelona', 'FCB', 'Spain', 1899, false, 'logo_url_bcn'),
+            'venue' => new VenueDTO(1, 'Camp Nou', 'C. dArístides Maillol, 12', 'Barcelona', 99354, 'grass', 'image_url_cn')
+        ];
+        $teamData2 = [
+            'team' => new TeamDTO(33, 'Manchester United', 'MUN', 'England', 1878, false, 'logo_url_mu'),
+            'venue' => new VenueDTO(2, 'Old Trafford', 'Sir Matt Busby Way', 'Manchester', 74879, 'grass', 'image_url_ot')
+        ];
+
+        $favoriteTeamServiceMock = Mockery::mock(FavoriteTeamService::class);
+        $favoriteTeamServiceMock->shouldReceive('getFavoriteTeams')->with($user->id)->andReturn($favoriteTeamIds);
+        $this->app->instance(FavoriteTeamService::class, $favoriteTeamServiceMock);
+
+        $footballDataServiceMock = Mockery::mock(FootballDataService::class);
+        $footballDataServiceMock->shouldReceive('getTeamById')->with(529)->andReturn($teamData1);
+        $footballDataServiceMock->shouldReceive('getTeamById')->with(33)->andReturn($teamData2);
+        $footballDataServiceMock->shouldReceive('getTeamsMatches')->with($favoriteTeamIds)->andReturn([]); // It will be overwritten
+        $this->app->instance(FootballDataService::class, $footballDataServiceMock);
+
+        $response = $this->get(route('dashboard'));
+
+        $response->assertOk();
+        $response->assertInertia(function (AssertableInertia $page) use ($teamData1, $teamData2) {
+            $page->component('dashboard')
+                ->has('favoriteTeams', 2)
+                ->where('favoriteTeams.0.team.id', $teamData1['team']->id)
+                ->where('favoriteTeams.0.team.name', $teamData1['team']->name)
+                ->where('favoriteTeams.0.venue.name', $teamData1['venue']->name)
+                ->where('favoriteTeams.1.team.id', $teamData2['team']->id)
+                ->where('favoriteTeams.1.team.name', $teamData2['team']->name)
+                ->where('favoriteTeams.1.venue.name', $teamData2['venue']->name)
+                ->has('matchData', 2); // Asserting based on the fake data from the controller
+        });
+    }
 }
